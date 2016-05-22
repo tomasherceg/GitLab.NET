d2d293653801d4167734913b7854d95ecca7d288
Anthony Stivers <tony.stivers@gmail.com>
2016-05-22 01:39:52 -0400

    Add NuGet package to solution
    Add nuspec file for nuget packaging information.
    Add commands to package and publish nuget package to gitlab-ci.

--- GitLabAPI/Properties/AssemblyInfo.cs
+++ GitLabAPI/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GitLabAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is a wrapper around GitLab's API that works with your .NET projects. This project is currently in Alpha phase, and as such it may not work correctly.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Anthony Stivers, Ryan Marshall")]
 [assembly: AssemblyProduct("GitLabAPI")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright Anthony Stivers ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.4.0")]
+[assembly: AssemblyFileVersion("0.0.4.0")]
