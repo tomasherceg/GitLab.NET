d5b9370601249467dabf787b9819928f4bfd5d4d
Anthony Stivers <tony.stivers@gmail.com>
2016-05-22 11:51:47 -0400

    Fix un-changed names
    Assembly information, assembly name, and default namespace were not
    changed when renaming the project. Change these to reflect the correct
    project name.

--- GitLabAPI.NET/Properties/AssemblyInfo.cs
+++ GitLabAPI.NET/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GitLabAPI")]
+[assembly: AssemblyTitle("GitLabAPI.NET")]
 [assembly: AssemblyDescription("This is a wrapper around GitLab's API that works with your .NET projects. This project is currently in Alpha phase, and as such it may not work correctly.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Anthony Stivers")]
-[assembly: AssemblyProduct("GitLabAPI")]
+[assembly: AssemblyProduct("GitLabAPI.NET")]
 [assembly: AssemblyCopyright("Copyright Anthony Stivers ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.5.0")]
-[assembly: AssemblyFileVersion("0.0.5.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
