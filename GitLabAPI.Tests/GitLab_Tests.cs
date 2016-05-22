d1b3b531b0a49ea2949b11a6e7955497961db300
Anthony Stivers <tony.stivers@gmail.com>
2016-05-22 09:59:17 -0400

    Implement base methods for GitLab class
    #7
    
    [ci skip]

---
+++ GitLabAPI.Tests/GitLab_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace GitLabAPI.Tests
+{
+    public class GitLab_Tests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Constructor_PrivateTokenNullOrEmpty_ThrowsArgumentNullException(string privateToken)
+        {
+            Assert.Throws<ArgumentNullException>(() => new GitLab(privateToken, new Uri("https://host.com")));
+        }
+    }
+}
---
+++ GitLabAPI/GitLab.cs
@@ -0,0 +1,60 @@
+﻿using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace GitLabAPI
+{
+    public class GitLab
+    {
+        public Uri HostUri { get; private set; }
+
+        private const string apiPath = "/api/v3";
+        private string privateToken;
+
+        public GitLab(string privateToken, Uri hostUri)
+        {
+            if (string.IsNullOrEmpty(privateToken))
+                throw new ArgumentNullException(nameof(privateToken));
+
+            this.privateToken = privateToken;
+            HostUri = new Uri(hostUri, apiPath);
+        }
+
+        /// <summary>
+        /// Retrieves a private token for a user.
+        /// </summary>
+        /// <param name="user">The username or email address for the user.</param>
+        /// <param name="password">The user's password.</param>
+        /// <returns>The user's private token. Null if the login failed.</returns>
+        public static async Task<string> GetPrivateToken(string user, string password, Uri hostUri)
+        {
+
+        }
+
+        public T Execute<T>(RestRequest request) where T : new()
+        {
+            var client = new RestClient();
+            client.BaseUrl = HostUri;
+
+            // Add the private token to every request
+            request.AddParameter("PRIVATE-TOKEN", privateToken, ParameterType.HttpHeader);
+
+            var response = client.Execute<T>(request);
+
+            return response.Data;
+        }
+
+        public async Task<T> ExecuteAsync<T>(RestRequest request) where T : new()
+        {
+            var client = new RestClient();
+            client.BaseUrl = HostUri;
+
+            // Add the private token to every request
+            request.AddParameter("PRIVATE-TOKEN", privateToken, ParameterType.HttpHeader);
+
+            var response = await client.ExecuteTaskAsync<T>(request);
+
+            return response.Data;
+        }
+    }
+}
