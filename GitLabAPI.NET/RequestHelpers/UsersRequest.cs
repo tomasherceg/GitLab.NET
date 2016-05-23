a01c50cfc6b4cab9cadc9a75e66f21b6e33f404b
Anthony Stivers <tony.stivers@gmail.com>
2016-05-22 22:22:41 -0400

    Add UsersRequest helper

---
+++ GitLabAPI.NET/RequestHelpers/UsersRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace GitLabAPI.NET.RequestHelpers
+{
+    public class UsersRequest : IRequestHelper
+    {
+
+
+        public RestRequest GetRequest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
