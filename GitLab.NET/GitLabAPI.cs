0e9380dd282cf41553af03471501ecf08a72555e
Anthony Stivers <tony.stivers@gmail.com>
2016-05-23 07:34:50 -0400

    Add basic GitLabAPI class
    This class is used for consumers who would like to access all parts of
    the api from a single base instance, rather than creating instances of
    GitLabBase subclasses based on their needs. We still allow both models
    for modularity and flexibility.

---
+++ GitLab.NET/GitLabAPI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLab.NET
+{
+    public class GitLabAPI
+    {
+        public GitLabUsers Users { get; private set; }
+
+        public GitLabAPI(string privateToken, Uri hostUri)
+        {
+            if (string.IsNullOrEmpty(privateToken))
+                throw new ArgumentNullException(nameof(privateToken));
+
+            if (hostUri == null)
+                throw new ArgumentNullException(nameof(hostUri));
+
+            Users = new GitLabUsers(privateToken, hostUri);
+        }
+    }
+}
