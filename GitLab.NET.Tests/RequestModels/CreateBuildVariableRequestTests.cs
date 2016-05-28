f5d9dd788553d7e5b03cb5a3f9bfcd476ab0a758
Anthony Stivers <tony.stivers@gmail.com>
2016-05-28 05:01:11 -0400

    Implement build variables API

---
+++ GitLab.NET.Tests/RequestModels/CreateBuildVariableRequestTests.cs
@@ -0,0 +1,81 @@
+﻿using GitLab.NET.RequestModels;
+using GitLab.NET.Tests.TestHelpers;
+using RestSharp;
+using Xunit;
+
+namespace GitLab.NET.Tests.RequestModels
+{
+    public class CreateBuildVariableRequestTests
+    {
+        [Fact]
+        public void GetRequest_AddsKeyParameter()
+        {
+            const string expected = "key";
+            var expectedParameter = new Parameter
+            {
+                Name = "key",
+                Value = expected,
+                Type = ParameterType.GetOrPost
+            };
+            var sut = new CreateBuildVariableRequest(0, expected, "value");
+
+            var result = sut.GetRequest();
+
+            Assert.Contains(expectedParameter, result.Parameters, new ParameterEqualityComparer());
+        }
+
+        [Fact]
+        public void GetRequest_AddsProjectIdParameter()
+        {
+            const uint expected = 5;
+            var expectedParameter = new Parameter
+            {
+                Name = "projectId",
+                Value = expected,
+                Type = ParameterType.UrlSegment
+            };
+            var sut = new CreateBuildVariableRequest(expected, "key", "value");
+
+            var result = sut.GetRequest();
+
+            Assert.Contains(expectedParameter, result.Parameters, new ParameterEqualityComparer());
+        }
+
+        [Fact]
+        public void GetRequest_AddsValueParameter()
+        {
+            const string expected = "value";
+            var expectedParameter = new Parameter
+            {
+                Name = "value",
+                Value = expected,
+                Type = ParameterType.GetOrPost
+            };
+            var sut = new CreateBuildVariableRequest(0, "key", expected);
+
+            var result = sut.GetRequest();
+
+            Assert.Contains(expectedParameter, result.Parameters, new ParameterEqualityComparer());
+        }
+
+        [Fact]
+        public void GetRequest_SetsCorrectResource()
+        {
+            var sut = new CreateBuildVariableRequest(0, "key", "value");
+
+            var result = sut.GetRequest();
+
+            Assert.Equal(CreateBuildVariableRequest.Resource, result.Resource);
+        }
+
+        [Fact]
+        public void GetRequest_SetsMethodToPost()
+        {
+            var sut = new CreateBuildVariableRequest(0, "key", "value");
+
+            var result = sut.GetRequest();
+
+            Assert.Equal(Method.POST, result.Method);
+        }
+    }
+}
---
+++ GitLab.NET/RequestModels/UpdateBuildVariableRequest.cs
@@ -0,0 +1,29 @@
+﻿using RestSharp;
+
+namespace GitLab.NET.RequestModels
+{
+    internal class UpdateBuildVariableRequest : IRequestModel
+    {
+        public const string Resource = "projects/{projectId}/variables/{key}";
+        private readonly string _key;
+
+        private readonly uint _projectId;
+        private readonly string _value;
+
+        public UpdateBuildVariableRequest(uint projectId, string key, string value)
+        {
+            _projectId = projectId;
+            _key = key;
+            _value = value;
+        }
+
+        public IRestRequest GetRequest()
+        {
+            var result = new RestRequest(Resource, Method.PUT);
+            result.AddParameter("projectId", _projectId, ParameterType.UrlSegment);
+            result.AddParameter("key", _key, ParameterType.UrlSegment);
+            result.AddParameter("value", _value);
+            return result;
+        }
+    }
+}
