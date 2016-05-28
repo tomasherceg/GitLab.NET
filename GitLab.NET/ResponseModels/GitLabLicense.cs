84aec4b9a851fee40eccbb8b515f5fcb1f6b2c78
Anthony Stivers <tony.stivers@gmail.com>
2016-05-27 23:17:35 -0400

    Implement GitLab license API

---
+++ GitLab.NET/ResponseModels/GitLabLicense.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GitLab.NET.ResponseModels
+{
+    public class GitLabLicense
+    {
+        /// <summary> The date and time this license starts at. </summary>
+        public DateTime? StartsAt { get; set; }
+
+        /// <summary> The date and time this license expires at. </summary>
+        public DateTime? ExpiresAt { get; set; }
+
+        /// <summary> The licensee. </summary>
+        public Licensee Licensee { get; set; }
+
+        /// <summary> The user limit for this license. </summary>
+        public uint? UserLimit { get; set; }
+
+        /// <summary> THe number of active users this license currently has. </summary>
+        public uint? ActiveUsers { get; set; }
+    }
+}
---
+++ GitLab.NET/ResponseModels/Licensee.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable UnusedMember.Global
+
+namespace GitLab.NET.ResponseModels
+{
+    /// <summary> Stores information about a GitLab licensee. </summary>
+    public class Licensee
+    {
+        /// <summary> The licensee's name. </summary>
+        public string Name { get; set; }
+
+        /// <summary> The licensee's company name. </summary>
+        public string Company { get; set; }
+
+        /// <summary> The licensee's email address. </summary>
+        public string Email { get; set; }
+    }
+}
