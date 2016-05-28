df8c60ed19ce3bd7a0e2c7e27be05587cdb4eaa4
Anthony Stivers <tony.stivers@gmail.com>
2016-05-28 11:50:34 -0400

    Update version for alpha release

--- GitLab.NET/Properties/AssemblyInfo.cs
+++ GitLab.NET/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
 
 // Set internal members visible to test class
 
