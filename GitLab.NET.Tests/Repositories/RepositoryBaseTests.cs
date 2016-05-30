baeb7a6fb39b946e6bcfea8a58dd6c244a878085
Anthony Stivers <tony.stivers@gmail.com>
2016-05-30 18:12:08 -0400

    Add tests for RepositoryBase

---
+++ GitLab.NET.Tests/Repositories/RepositoryBaseTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GitLab.NET.Abstractions;
+using GitLab.NET.Repositories;
+using Xunit;
+
+namespace GitLab.NET.Tests.Repositories
+{
+    public class RepositoryBaseTests
+    {
+        private class TestRepository : RepositoryBase
+        {
+            public TestRepository(IRequestFactory requestFactory) : base(requestFactory) { }
+        }
+
+        [Fact]
+        public void Constructor_RequestFactoryIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new TestRepository(null));
+        }
+    }
+}
