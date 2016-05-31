04d118120fca1cb7f1bb92c9b0ed92746d334f74
Anthony Stivers <tony.stivers@gmail.com>
2016-05-31 07:07:30 -0400

    Add unit tests for SessionRepository

---
+++ GitLab.NET.Tests/Repositories/SessionRepositoryTests.cs
@@ -0,0 +1,70 @@
+﻿using NSubstitute;
+using System;
+using System.Threading.Tasks;
+using GitLab.NET.Abstractions;
+using GitLab.NET.Repositories;
+using Xunit;
+
+namespace GitLab.NET.Tests.Repositories
+{
+    public class SessionRepositoryTests
+    {
+        public SessionRepositoryTests()
+        {
+            _request = Substitute.For<IRequest>();
+            _requestFactory = Substitute.For<IRequestFactory>();
+            _requestFactory.Create(Arg.Any<string>(), Arg.Any<Method>(), Arg.Any<bool>()).Returns(_request);
+        }
+
+        private readonly IRequest _request;
+        private readonly IRequestFactory _requestFactory;
+
+        [Fact]
+        public async Task RequestSession_UsernameIsNull_ThrowsArgumentNullException()
+        {
+            var sut = new SessionRepository(_requestFactory);
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => sut.RequestSession(null, "password"));
+        }
+
+        [Fact]
+        public async Task RequestSession_PasswordIsNull_ThrowsArgumentNullException()
+        {
+            var sut = new SessionRepository(_requestFactory);
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => sut.RequestSession("username", null));
+        }
+
+        [Fact]
+        public async Task RequestSession_ValidParameters_AddsLoginParameter()
+        {
+            const string expected = "username";
+            var sut = new SessionRepository(_requestFactory);
+
+            await sut.RequestSession(expected, "password");
+
+            _request.Received().AddParameter("login", expected);
+        }
+
+        [Fact]
+        public async Task RequestSession_ValidParameters_AddsPasswordParameter()
+        {
+            const string expected = "password";
+            var sut = new SessionRepository(_requestFactory);
+
+            await sut.RequestSession("username", expected);
+
+            _request.Received().AddParameter("password", expected);
+        }
+
+        [Fact]
+        public async Task RequestSession_ValidParameters_SetsCorrectResourceAndMethod()
+        {
+            var sut = new SessionRepository(_requestFactory);
+
+            await sut.RequestSession("username", "password");
+
+            _requestFactory.Received().Create("session", Method.Post, false);
+        }
+    }
+}
