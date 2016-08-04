5f177f7ad185342229a86a87b6cbf93195d77bc4
zinovyev.petr <petr.zinovyev@devexpress.com>
2016-08-04 16:21:05 +0300

    remove test that for internal use only

--- GitLab.NET.Tests/Repositories/ProjectRepositoryTests.cs
+++ GitLab.NET.Tests/Repositories/ProjectRepositoryTests.cs
@@ -27,30 +27,6 @@ namespace GitLab.NET.Tests.Repositories {
 
             _request.Received().AddUrlSegment("id", expected);
         }
-
-        [Fact]
-        public async Task GetProject() {
-            GitLabClient client = new GitLabClient(new Uri(TestCredentials.GitServer), TestCredentials.GitLabToken);
-            var data = (await client.Projects.GetAll()).Data;
-            foreach(var el in data) {
-                System.Diagnostics.Debug.WriteLine(el.Name);
-            }
-            data = (await client.Projects.Accessible()).Data;
-            foreach(var el in data) {
-                System.Diagnostics.Debug.WriteLine(el.Name);
-            }
-            var res = (await client.Projects.GetAll()).Data.FirstOrDefault(x => string.Compare(x.HttpUrlToRepo, TestCredentials.GitServer + "/tester/testxpfall", StringComparison.InvariantCultureIgnoreCase) == 0);
-
-            //GitLabWrapper wrapper = new GitLabWrapper(TestCredentials.GitServer, TestCredentials.GitLabToken);
-            //var project = wrapper.FindProject("tester/testxpfall");
-            //Assert.IsNotNull(project);
-        }
-    }
-
-    public static class TestCredentials {
-        public const string GitLabToken = "X6XV2G_ycz_U4pi4m93K";
-        public const string GitServer = "http://litvinov-lnx";
-        public const string VcsServer = @"net.tcp://vcsservice.devexpress.devx:9091/DXVCSService";
     }
 
 
